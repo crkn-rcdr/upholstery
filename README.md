feb0ac2ee830e2a75c5eb49fb5588427b9334a2d
Sascha Adler <sascha.adler@canadiana.ca>
2018-05-30 14:02:29 -0400

    Initial code commit. First iteration of upholstery simply translates the request to the Couch endpoint after processing it through cihm-jwt middleware.

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # upholstery
 
 Authorization and abstraction layer sitting on top of CouchDB. Get it?
+
+## Configuration
+
+The service currently expects a `config.json` file in the root directory, with the following contents:
+
+    {
+        "couch": $COUCHDB_URL,
+        "timeout": $DEFAULT_COUCH_REQUEST_TIMEOUT
+        "secrets": {"$SECRET_KEY_ID": "$SECRET_KEY"}
+    }
+    
---
+++ src/server.js
@@ -0,0 +1,39 @@
+const Koa = require('koa');
+const rp = require('request-promise-native');
+const cors = require('kcors');
+const onerror = require('koa-onerror');
+const jwt = require('cihm-jwt');
+
+const app = new Koa();
+
+const config = require(process.env.APP_CONFIG || '../config');
+
+const DEFAULT_TIMEOUT = 5000;
+const couch = rp.defaults({
+    baseUrl: config.couch,
+    headers: { Accept: 'application/json' },
+    json: true,
+    timeout: config.timeout || DEFAULT_TIMEOUT,
+    simple: false, // promise accepted for more than just 2xx responses
+    resolveWithFullResponse: true
+});
+
+onerror(app);
+
+app.use(cors({ origin: '*' }));
+
+app.use(jwt(config.secrets));
+
+app.use(async ctx => {
+    try {
+        let response = await couch({ method: ctx.method, uri: ctx.path });
+        ctx.body = response.body;
+        ctx.status = response.statusCode;
+    } catch(e) {
+        ctx.body = e;
+        ctx.status = e.statusCode || 500;
+    }
+});
+
+app.listen(3000);
+
