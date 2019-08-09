a0ddc0e5b8e78f9dd891dd862d21a26bfa0441da
Sascha Adler <sadler@crkn.ca>
2019-08-09 17:16:41 -0400

    Handles CORS pre-flight OPTIONS requests; handles CORS in general. Perhaps a bit too permissive.

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const validateJwt = require("./jwt");
 
 const proxy = httpProxy.createProxyServer({});
 
+proxy.on("proxyRes", (_proxyRes, _req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+});
+
 https
   .createServer(
     {
@@ -16,18 +20,28 @@ https
       key: fs.readFileSync(__dirname + "/ssl/server.key")
     },
     (req, res) => {
-      let [jwtValid, message] = validateJwt(req);
-      if (jwtValid) {
-        if (url.parse(req.url).pathname === "/cookie") {
-          res.setHeader("Set-Cookie", [`auth_token=${message}`]);
-          res.writeHead(302, { Location: "/_utils" });
-          res.end();
-        } else {
-          proxy.web(req, res, { target: env.require("COUCH") });
-        }
+      if (req.method === "OPTIONS") {
+        // this is a pre-flight CORS request, which will happen if the browser is trying to set the Authorization header
+        res.writeHead(204, {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "*",
+          "Access-Control-Allow-Headers": "*"
+        });
+        res.end();
       } else {
-        res.writeHead(401, { "Content-Type": "text/plain" });
-        res.end(`JWT authenication failure: ${message}`);
+        let [jwtValid, message] = validateJwt(req);
+        if (jwtValid) {
+          if (url.parse(req.url).pathname === "/cookie") {
+            res.setHeader("Set-Cookie", [`auth_token=${message}`]);
+            res.writeHead(302, { Location: "/_utils" });
+            res.end();
+          } else {
+            proxy.web(req, res, { target: env.require("COUCH") });
+          }
+        } else {
+          res.writeHead(401, { "Content-Type": "text/plain" });
+          res.end(`JWT authenication failure: ${message}`);
+        }
       }
     }
   )
